4ae37dd9891312c16a5b8fb41e51b000aeaef07a
Xuanyu Zhou <zhou@xuanyu.me>
2016-12-10 23:41:50 -0600

    finished raw naive bayes

--- nb.cpp
+++ nb.cpp
@@ -3,8 +3,8 @@
 #include <map>
 #include <vector>
 #include <string>
+#include <algorithm>
 using namespace std;
-
 vector<string> split_by_space(string in){
 	int base = 0;
 	vector<string> ret;
@@ -62,7 +62,12 @@ int build_database(char* filename, map<int, map<string, int> > &freq_map, map<in
 		getline(input, curtest);
 		testinfo.push_back(curtest);
 	}
+
+	input.close();
 	return 1;
+}
+bool pair_comparator(pair<int, double> a, pair<int, double> b){
+	return a.second > b.second;
 }
 int main(int argc, char** argv){
 	if (argc < 3){
@@ -79,20 +84,35 @@ int main(int argc, char** argv){
 	if (!build_database(input_file_name, freq_map, count_map, testinfo, total_sample, total_test)){
 		return 1;
 	}
+	ofstream output;
+	output.open(output_file_name);
 	double bias = (double)count_map.size();
 	for(int i = 0; i < total_test; i++){
 		vector<string> curvec = split_by_space(testinfo[i]);
-		map<int, double> prob_map;
+		vector< pair<int, double> > prob_pair;
 		for(auto it = count_map.begin(); it != count_map.end(); it++){
 			int label = it->first;
 			map<string, int> curfreq= freq_map[label];
-			double bias_up = bias * (double)count_map[label] / (double)total_sample;	
+			double labelCount = (double)count_map[label];
+			double bias_up = bias * labelCount / (double)total_sample;	
+			double wordProb = labelCount / (double)total_sample;
 			for(int j = 0; j < curvec.size(); j++){
-				double prob_cur_word = (double)prob
+				double prob_cur_word = ((double)(curfreq[curvec[j]]) + bias_up) / (labelCount + bias);
+				wordProb *= prob_cur_word;
 			}
-			
+			prob_pair.push_back(make_pair(label, wordProb));
 		}
+		sort(prob_pair.begin(), prob_pair.end(), pair_comparator);
+
+		for(int oi = 0; oi < 5; oi++){
+			output << prob_pair[oi].first;
+			if (oi != 4)
+				output << " ";
+		}
+		output << "\n";
+	
 	}
+	output.close();
 
 
 	return 0;
