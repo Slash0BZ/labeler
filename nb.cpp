47a3c8d6455835f63494aa5120eaba887109136d
Xuanyu Zhou <zhouxuanyu.personal@gmail.com>
2016-12-13 15:00:37 -0600

    changed to stdin

--- nb.cpp
+++ nb.cpp
@@ -1,3 +1,8 @@
+/*
+ *An Naive Bayes approach on sentence classifier.
+ *Use Multinominol and Bag of Words model.
+ *Utilizes term freqency and smoothing.
+ */
 #include <iostream>
 #include <fstream>
 #include <map>
@@ -6,6 +11,12 @@
 #include <algorithm>
 #include <cmath>
 using namespace std;
+/*
+ *Split sentences by space.
+ *Ignores extra spaces.
+ *Parameter: a string of sentence
+ *Output: a vector contains splitted words.
+ */
 vector<string> split_by_space(string in){
 	int base = 0;
 	vector<string> ret;
@@ -21,9 +32,12 @@ vector<string> split_by_space(string in){
 	ret.push_back(in.substr(base, in.size() - base));
 	return ret;
 }
+/*
+ *Process question string
+ *Deals with 's and extra spaces.
+ */
 void processQuestionString(string &q){
 	for(int i = 0; i < q.length(); i++){
-		//if (q[i] == '?' || q[i] == '\'' || q[i] == ',' || q[i] == '/'){
 		if (q[i] == '\'' && q[i+1] == 's'){
 			q[i] = ' ';
 			q[i+1] = ' ';
@@ -39,29 +53,46 @@ void processQuestionString(string &q){
 		q.erase(lastNonSpace + 1, q.length() - lastNonSpace - 1);
 	}
 }
+/*
+ *An experiment that tests which 
+ *segment the word is in, thus adding
+ *extra info regarding positioning.
+ */
 string getSegment(int index, int size){
 	int segments = 3;
 	int segmentSize = size / 3;
 	int segment = segmentSize != 0 ? index / segmentSize : 0;
 	return to_string(segment);
 }
+/*
+ *Process test data. Use two maps to store info.
+ *count_map: counts of each labels.
+ *freq_map: stores label->word->count.
+ */
 int build_database(char* filename, map<int, map<string, int> > &freq_map, map<int, int> &count_map, vector<string> &testinfo, int &total_sample, int &total_test){
+/*
 	ifstream input;
 	input.open(filename);
 	if (!input.is_open()){
 		cout << "error: in reading input file \"" << filename << "\"" << endl;
 		return 0;
 	}
+*/
 	string info;
-	getline(input, info);
+	//getline(input, info);
+	getline(cin, info);
 	vector<string> infovec = split_by_space(info);
 	total_sample = stoi(infovec[0]);
 	total_test = stoi(infovec[1]);
 	for(int i = 0; i < total_sample; i++){
 		string label;
 		string question;
+		/*
 		getline(input, label);
 		getline(input, question);
+		*/
+		getline(cin, label);
+		getline(cin, question);
 
 		processQuestionString(question);
 
@@ -78,35 +109,45 @@ int build_database(char* filename, map<int, map<string, int> > &freq_map, map<in
 			}
 			for(int k = 0; k < question_vec.size(); k++){
 				string segment = getSegment(k, question_vec.size());
+				map<string, int> found;
 				string curWord = question_vec[k];
-				if (freq_map[label_index].find(curWord) != freq_map[label_index].end()){
-					freq_map[label_index][curWord] += 1;
+				if (found.find(curWord) == found.end()){
+					if (freq_map[label_index].find(curWord) != freq_map[label_index].end()){
+						freq_map[label_index][curWord] += 1;
+					}
+					else{
+						freq_map[label_index][curWord] = 1;
+					}
+					found[curWord] = 1;
 				}
 				else{
-					freq_map[label_index][curWord] = 1;
+					cout << 1 << endl;
 				}
 			}
 		}
 	}
 	for(int i = 0; i < total_test; i++){
 		string curtest;	
-		getline(input, curtest);
+		//getline(input, curtest);
+		getline(cin, curtest);
 		testinfo.push_back(curtest);
 	}
 
-	input.close();
+	//input.close();
 	return 1;
 }
 bool pair_comparator(pair<int, double> a, pair<int, double> b){
 	return a.second > b.second;
 }
 int main(int argc, char** argv){
+	char *input_file_name = NULL;
+	char *output_file_name = NULL;
 	if (argc < 3){
-		cout << "usage: ./nb [input_file] [output_file]" << endl;
-		return 1;
 	}
-	char* input_file_name = *(argv + 1);
-	char* output_file_name = *(argv + 2);
+	else{
+		char* input_file_name = *(argv + 1);
+		char* output_file_name = *(argv + 2);
+	}
 	
 	map<int, map<string, int> > freq_map;
 	map<int, int> count_map;
@@ -115,8 +156,10 @@ int main(int argc, char** argv){
 	if (!build_database(input_file_name, freq_map, count_map, testinfo, total_sample, total_test)){
 		return 1;
 	}
+	/*
 	ofstream output;
 	output.open(output_file_name);
+	*/
 	double bias = (double)count_map.size();
 	for(int i = 0; i < total_test; i++){
 		vector<string> curvec = split_by_space(testinfo[i]);
@@ -131,6 +174,8 @@ int main(int argc, char** argv){
 				string curWord = curvec[j];
 				auto pos = curfreq.find(curWord);
 				if (pos != curfreq.end()){
+					//The commented out line is an implementation of term freqency.
+					//double prob_cur_word = (((double)(pos->second) + 1) * log((double)total_sample / (double)(pos->second))) / (labelCount + bias + 1);
 					double prob_cur_word = ((double)(pos->second) + 1) / (labelCount + bias + 1);
 					wordProb += log(prob_cur_word);
 				}
@@ -144,15 +189,14 @@ int main(int argc, char** argv){
 		sort(prob_pair.begin(), prob_pair.end(), pair_comparator);
 
 		for(int oi = 0; oi < 10; oi++){
-			//output << prob_pair[oi].first << ": " << prob_pair[oi].second << "; ";
-			output << prob_pair[oi].first;
+			cout << prob_pair[oi].first;
 			if (oi != 4)
-				output << " ";
+				cout << " ";
 		}
-		output << "\n";
+		cout << "\n";
 	
 	}
-	output.close();
+	//output.close();
 
 
 	return 0;
